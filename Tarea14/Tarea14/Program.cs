8ba7a0aa008b66f8f127105b4f09dd49eceab728
Matias Salatino <mesalatino88@gmail.com>
2021-09-20 15:47:09 -0300

    Tareas

---
+++ Tarea14/Tarea14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tarea14
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int a=1;
+            while (a < 11)
+            {
+                Console.WriteLine(a);
+                a = a + 1;
+            }
+        }
+    }
+}
---
+++ Tarea16/Tarea16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarea15
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int a;
+
+            for (a=1;a<11;a++)
+            {
+                Console.WriteLine(a);
+                
+            }
+            
+        }
+
+    }
+}
