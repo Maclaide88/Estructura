b9910f2d5a164ebbd7bd072c49fad2f62505c779
Matias Salatino <mesalatino88@gmail.com>
2021-09-20 15:57:56 -0300

    Tareas

---
+++ Ejercicios 1/Tarea10/Tarea10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea7
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int a;
+            int b;
+            Console.WriteLine("Ingrese el primer numero");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+                Console.WriteLine("El numero {0} es par", a);
+            else
+                Console.WriteLine("El numero {0} es impar", a);
+        }
+    }
+}
---
+++ Ejercicios 1/Tarea5/Tarea5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tarea5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("El resultado de (20+5) % 6 es: {0}", (20 + 5) % 6);
+        }
+    }
+}
