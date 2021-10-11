4930685b52c5e7b0b6f4a449345ed6e91c16fc15
Matias Salatino <mesalatino88@gmail.com>
2021-10-11 20:47:31 -0300

    Tarea

---
+++ Tarea 2/Tarea3/Tarea3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 9;
+            Console.WriteLine("La tabla del 9 es:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + "x" + i + "=" + (i * number));
+            }
+
+        }
+    }
+}
+
