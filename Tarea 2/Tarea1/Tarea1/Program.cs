0d9786f1e7a64f786652f4df70e2c1a2b1485120
Matias Salatino <mesalatino88@gmail.com>
2021-10-11 20:35:22 -0300

    Tarea 2

---
+++ Tarea 2/Tarea1/Tarea1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tarea1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int id;
+            int clave;
+
+            Console.WriteLine("Introduzca su Identeficador: ");
+            id = Convert.ToInt32(Console.ReadLine());
+
+            while (id != 1234)
+            {
+
+                Console.WriteLine("Su identificacion es erronea, vuelva a intentar");
+                Console.WriteLine("Introduzca su Identeficador: ");
+                id = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Introduzca su clave: ");
+            clave = Convert.ToInt32(Console.ReadLine());
+
+            while (clave != 1111)
+            {
+
+                Console.WriteLine("Su clave es erronea, vuelva a intentar");
+                Console.WriteLine("Introduzca su clave: ");
+                clave = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("EXITO!");
+        }
+    }
+}
