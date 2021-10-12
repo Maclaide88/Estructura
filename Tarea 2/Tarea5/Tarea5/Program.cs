b4f8f2a30088c0995483d1e58833a98d03f3f4fe
Matias Salatino <mesalatino88@gmail.com>
2021-10-11 21:02:54 -0300

    Tarea

---
+++ Tarea 2/Tarea5/Tarea5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tarea4
+{
+    class Program
+    {
+        static void Main()
+        {
+            string f;
+            f = "Hola";
+            for (; ; )
+            {
+                Console.Write("{0}", f);
+            }
+        }
+    }
+}
