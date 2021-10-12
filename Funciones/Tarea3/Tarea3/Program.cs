7fa80205c0d5be81ba387766be3d6ce8da4410e0
Matias Salatino <mesalatino88@gmail.com>
2021-10-12 00:04:38 -0300

    Tarea

---
+++ Funciones/Tarea3/Tarea3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tarea3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numero;
+            bool resultado;
+            Console.WriteLine("Ingrese un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            resultado = EsPrimo(numero);
+            if (resultado == true)
+            {
+                Console.WriteLine("El numero es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es primo");
+            }
+        }
+
+        public static bool EsPrimo(int numero)
+        {
+            int i;
+            bool resultado;
+            for (i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    resultado= true;
+                }
+            }
+
+        }
+    }
+}
---
+++ Funciones/Tarea3/Tarea3/ProgramBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Tarea3
+{
+    internal class ProgramBase
+    {
+
+        public static bool EsPrimo(int numero)
+        {
+
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return true;
+                }
+            }
+
+        }
+    }
+}
