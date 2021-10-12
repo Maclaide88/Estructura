6290d962d4fbed4f4ce6686c7b8a06f0852545a0
Matias Salatino <mesalatino88@gmail.com>
2021-10-12 01:22:27 -0300

    Tarea

---
+++ Funciones/Tarea7/Tarea7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tarea7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese una letra");
+            string l = Console.ReadLine();
+            Console.WriteLine("Ingrese una numero");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i >= 0; i--)
+            {
+                for (int b = 0; b < i; b++)
+                {
+                    Console.Write("{0}", l);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
