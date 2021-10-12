e8565942284f0b255ad4ced699e3dd46ff12d69b
Matias Salatino <mesalatino88@gmail.com>
2021-10-11 22:07:07 -0300

    Tarea

---
+++ Tarea 2/Tarea10/Tarea10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tarea10
+{
+    class Program
+    {
+        static void Main()
+        {
+            string usuario;
+            Console.WriteLine("Ingrese su nombre de Usuario: ");
+            usuario=Console.ReadLine();
+            if (usuario == "Martin")
+            {
+                Console.WriteLine("Hola");
+            }
+            else
+            {
+                Console.WriteLine("No te conozco");
+            }
+
+        }
+    }
+}
