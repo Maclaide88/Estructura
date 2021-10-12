2c4fa402b57bc14bcd313db16631a45c3752ce35
Matias Salatino <mesalatino88@gmail.com>
2021-10-12 01:03:59 -0300

    Tarea

---
+++ Funciones/Tarea6/Tarea6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tarea6
+{
+    class Program
+    {
+        static void Main()
+        {
+            string numeros;
+
+            Console.WriteLine("Escribe un número: ");
+            numeros = Console.ReadLine();
+
+            Console.WriteLine("\nLa suma total de sus cifras es {0}.", SumaCifras(numeros));
+            
+        }
+
+        public static int SumaCifras(string n)
+        {
+            int suma = 0;
+            for (int i = 0; i <= (n.Length - 1); i++)
+            {
+                suma += Convert.ToInt32(n[i].ToString());
+            }
+            return suma;
+        }
+    }
+}
