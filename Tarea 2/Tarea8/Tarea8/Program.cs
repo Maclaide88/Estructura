b50568ef6d04a9b9d65e9b338fbf659bfb49b55d
Matias Salatino <mesalatino88@gmail.com>
2021-10-11 21:54:44 -0300

    Tarea

---
+++ Tarea 2/Tarea8/Tarea8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tarea8
+{
+    class Program
+    {
+        static void Main()
+        {
+            string l;
+            Console.WriteLine("Introduzca una Letra");
+            l = Console.ReadLine();
+            if (l=="a" || l=="e" || l == "i" || l == "o" || l == "u")
+            {
+                Console.WriteLine("La letra es una Vocal");
+            }
+            else
+            {
+                Console.WriteLine("La letra NO es una Vocal");
+            }
+        }
+    }
+}
