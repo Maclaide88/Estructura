52451d94bee0e9ad0a43f32174325b2fdddfeabf
Matias Salatino <mesalatino88@gmail.com>
2021-10-11 21:49:14 -0300

    Tarea

---
+++ Tarea 2/Tarea7/Tarea7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tarea7
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;
+            a = 5;
+            b = a + 2;
+            b -= 3;
+            c = -3;
+            c *= 2;
+            ++c;
+            a *= b;
+
+            Console.WriteLine("El resultado de a es: {0}, de b es: {1} y de c es: {2}", a, b, c);
+        }
+    }
+}
