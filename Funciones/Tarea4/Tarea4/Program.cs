bffabe1f2f1b47f2395caac16832db9d5438fbe8
Matias Salatino <mesalatino88@gmail.com>
2021-10-12 00:13:11 -0300

    Tarea

---
+++ Funciones/Tarea4/Tarea4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tarea4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Clear();
+
+            // dibujo de cada linea (bucle externo)
+            for (int fila = 1; fila <= 7; fila++)
+
+            {
+                //díbuja espacios en blanco (ler bucle interno)
+                for (int espacios = 7 - fila; espacios > 0; espacios--)
+                    Console.Write(" ");// espai en blanc
+
+                // dibuja estrellas (2* bucle interno)
+                for (int conta = 1; conta < (2 * fila); conta++)
+                    Console.Write("*");
+
+                Console.WriteLine(" *");
+            }
+        }
+    }
+}
