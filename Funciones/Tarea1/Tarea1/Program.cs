8f2164bc435123d29711a65d82dae72de2b6dc0c
Matias Salatino <mesalatino88@gmail.com>
2021-10-11 23:16:38 -0300

    Tarea

---
+++ Funciones/Tarea1/Tarea1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tarea1
+{
+    class Program
+    {
+        static void Main()
+        {
+            float numero;
+            Console.WriteLine("Ingrese un numero real (-1,1,0)");
+            numero = (float)Convert.ToDouble(Console.ReadLine());
+            Signo(numero);
+        }
+
+        public static void Signo(float numero)
+        {
+            if (numero==-1)
+            {
+                Console.WriteLine("El numero ingresado es Negativo");
+            }
+            else if (numero==1)
+            {
+                Console.WriteLine("El numero ingresado es Positivo");
+            }
+            else if (numero == 0)
+            {
+                Console.WriteLine("El numero ingresado es Cero");
+            }
+        }
+    }
+}
